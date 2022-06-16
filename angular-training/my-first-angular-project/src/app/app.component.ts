3402811f60290e788e50a81ac38e603314be1ec2
rohancjadhav <rohancjadhav4@gmail.com>
2022-06-16 15:34:24 +0530

    small change

--- angular-training/my-first-angular-project/src/app/app.component.ts
+++ angular-training/my-first-angular-project/src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'this is my first app';
+  subtitle= 'this is new components title';
+ 
 }
